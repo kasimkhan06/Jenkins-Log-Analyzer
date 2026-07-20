e6f614bf686f8303e80a4c9ebaec63f2b4bb419e
kasim khan <kasimkkhan06@gmail.com>
2026-07-20 17:13:58 +0530

    added pydantic

--- src/jenkins_client.py
+++ src/jenkins_client.py
@@ -1,15 +1,20 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel , Field
-from typing import List
 load_dotenv()
 
 class JenkinsURL(BaseModel):
 
     job_base_url : str = Field(
-        description = ''
+        description = 'Base url of the jenkins job',
+    )
+    job_name : str = Field(
+        description='Name of the Job'
+    )
+    job_number : str = Field(
+        description='Number of the job executed'
     )
 
-def parse_jenkins(url : str) -> dict :
+def parse_jenkins(url : str) -> JenkinsURL :
 
     """
         Returns the job_path and build_number when passed an entire Jenkins URL
@@ -19,13 +24,16 @@ def parse_jenkins(url : str) -> dict :
     job_name = url.split('job/')[1].split('/')[0]
     job_number = url.split('job/')[1].split('/')[1]
 
-    url_info = {
-        'base_url' : job_base_url,
-        'job_name' : job_name,
-        'job_number' : job_number
-    }
+    jenkins_info = JenkinsURL(
+        job_base_url= job_base_url,
+        job_name=job_name,
+        job_number=job_number
+    )
 
-    return url_info
+    return jenkins_info
 
 
-print(parse_jenkins(url="http://chipd007.chennai.visteon.com:9090/view/all/job/Ford_ACM_Klocwork/1688/"))
+if __name__ == "__main__":
+
+    info = parse_jenkins(url = "http://chipd007.chennai.visteon.com:9090/job/Ford_ACM_Klocwork/1688/")
+    print(info)
